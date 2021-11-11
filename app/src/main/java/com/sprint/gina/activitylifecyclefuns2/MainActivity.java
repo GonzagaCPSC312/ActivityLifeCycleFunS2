9f97d7b0caf03a8b5bfea00037cd58454551361a
Gina Sprint <sprint@gonzaga.edu>
2021-11-11 11:30:35 -0800

    save instance state

--- app/src/main/java/com/sprint/gina/activitylifecyclefuns2/MainActivity.java
+++ app/src/main/java/com/sprint/gina/activitylifecyclefuns2/MainActivity.java
@@ -7,14 +7,17 @@ package com.sprint.gina.activitylifecyclefuns2;
 
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Locale;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivityTag";
     // count the number of seconds that have passed
     private int seconds = 0;
     // accumulate seconds?
@@ -25,11 +28,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // BE AWARE OF THE ACTIVITY LIFECYCLE
+        if (savedInstanceState != null) {
+            // restore running and seconds
+            running = savedInstanceState.getBoolean("running");
+            seconds = savedInstanceState.getInt("seconds");
+        }
 
+        // BE AWARE OF THE ACTIVITY LIFECYCLE
+        // check out the google drive notes for a table of the different
+        // lifecycle states and callback methods and descriptions
+        // for example: on device orientation change (e.g. portrait -> landscape)
+        // the app is destroyed and recreated
+        // define a layout-land resource directory, you can specify the layouts
+        // to be loaded on configuration change
+        // for example: on locale change (e.g. USA -> France)
+        // define a values-fr to store french specific (e.g. strings)
+//        https://developer.android.com/guide/topics/resources/runtime-changes
+
+        // onCreate() is a lifecycle method
+        Log.d(TAG, "onCreate: ");
 
         // start the timer. we only update seconds if running is true though
         runTimer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // we can save our activity instance state and view state here
+        // android will save *some* instance state for you (like some views)
+        // android will not save any view state for views without ids
+        // let's save our running and seconds attribute values in outState
+        // like intents, bundles save key-value pairs
+        
+        outState.putBoolean("running", running);
+        outState.putInt("seconds", seconds);
+        Log.d(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
     }
 
     // startButton click handler
